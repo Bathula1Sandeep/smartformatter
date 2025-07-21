a4c77debb8c5862d3e3fdda39e85b04ca67f1fdb
Bathula1Sandeep <sandeepbathula1@gmail.com>
2025-07-21 10:00:11 +0530

    First commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name='smartformater',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=['inflect'],
+    author='sandeep',
+    description='Utility fuction for smart formating of names, phones, numbers.',
+)
